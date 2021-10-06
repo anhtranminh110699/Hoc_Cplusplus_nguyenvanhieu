e75f6a00e504aaa3cf2faadbfd823cb865fcf970
Minh Anh Tran <anhtranminh110699@gmail.com>
2021-10-06 14:26:17 +0700

    Input and Output in C++

---
+++ InputAndOutput.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+	cout << "Nhap vao mot so nguyen: ";
+	cin >> number;
+	cout << number << "\n";
+
+	float weight;
+	cout << "Nhap vao can nang: ";
+	cin >> weight;
+	cout << weight << "\n";
+
+	string hello;
+	cout << "Nhap vao loi chao: ";
+	cin >> hello;
+	cout << hello << "\n";
+
+	return 0;
+}
